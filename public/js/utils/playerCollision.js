cbd7eb0e9a45c0d966c8f7d0d3000cdab60a0adb
GreatGonthar <60307580+GreatGonthar@users.noreply.github.com>
2023-12-30 12:38:37 +1000

    added a flag to prevent duplicate code execution

--- public/js/utils/playerCollision.js
+++ public/js/utils/playerCollision.js
@@ -1,22 +1,18 @@
 export const playerCollision = (myPlayer, players, socket) => {
-    
+    let flag = false
     for(let ID in players ){  
     let otherPlayer = players[ID]; 
-    const distance = Math.sqrt(
+    let distance = Math.sqrt(
         (otherPlayer.x - myPlayer.x) ** 2 +
             (otherPlayer.y - myPlayer.y) ** 2
             );    
             if (
-                distance < myPlayer.r &&
-                distance > 0 &&
-                otherPlayer.r < myPlayer.r / 2
-                ) {   
-                let myPlayerId = myPlayer.id 
-                let otherPlayerId = otherPlayer.id  
-                otherPlayer.r = 10
-                otherPlayer.x = Math.random() * 1000 * 2;
-                otherPlayer.y = Math.random() * 1000 * 2;              
-        socket.emit("dellPlayer", {myPlayerId, otherPlayerId});
+                distance < myPlayer.r  && otherPlayer.r < myPlayer.r/2 && !flag
+                ) {                 
+
+                console.log("client collision", ID)                   
+                socket.emit("dellPlayer", ID);
+                flag = true
     }
 }
 };
